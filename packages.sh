b5c29a10079d1a8782dc507749c9a9a1223c94c4
Andres <59713551+afcaballero-1994@users.noreply.github.com>
2025-02-11 21:32:21 -0500

    removed extra packages

--- packages.sh
+++ packages.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-dnf install -y fuse selinux-policy git distrobox neovim emacs ffmpeg-free ffmpegthumbnailer
+dnf install -y fuse selinux-policy git distrobox neovim emacs ffmpeg-free
 
 dnf install -y --nobest @"KDE Plasma Workspaces"
 #systemctl enable sddm.service
