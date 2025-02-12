0fc9cb070a239c73bc4fafbaa9a364d0df36c046
Andres Pedraza <andrespedca@gmail.com>
2025-02-11 23:04:27 -0500

    divided process two images one specifically for the nvidia base, should be easier to use different one

--- packages.sh
+++ packages.sh
@@ -2,7 +2,7 @@
 
 set -ouex pipefail
 
-dnf install -y fuse selinux-policy git distrobox neovim emacs ffmpeg-free
+dnf install -y fuse selinux-policy git distrobox neovim emacs ffmpeg-free firefox
 
 dnf install -y --nobest @"KDE Plasma Workspaces"
-#systemctl enable sddm.service
+systemctl enable sddm.service
