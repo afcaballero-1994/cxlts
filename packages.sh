08486859b47524fa67d2043380f7c060db3ff4fb
Andres <59713551+afcaballero-1994@users.noreply.github.com>
2025-02-27 11:22:41 -0500

    use gnome desktop while plasma works on almalinux

--- packages.sh
+++ packages.sh
@@ -3,6 +3,9 @@
 set -ouex pipefail
 
 dnf install -y fuse selinux-policy git distrobox neovim emacs
+#this does not work using almalinux
+#dnf install -y --nobest @"KDE Plasma Workspaces"
 
-dnf install -y --nobest @"KDE Plasma Workspaces"
-systemctl enable sddm.service
+dnf install -y @"Workstation"
+
+systemctl enable gdm.service
